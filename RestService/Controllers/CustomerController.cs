f8630558d4aee77a01da86cc80100cc84f3fc94f
Jakob Farian Krarup <kr@rup.dk>
2024-09-19 19:46:35 +0200

    Add project files.

---
+++ RestService/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestService.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        //a list of customers which we make static to save them on the class
+        //and make them persistent between calls to the controller
+        //this is to simulate a storage medium like a database
+        private static List<CustomerDto> customers = new List<CustomerDto>() { new CustomerDto() { Id = 1, Name = "Anna", Email = "Anna@gmail.com" }, new CustomerDto() { Id = 2, Name = "Bob", Email = "Bob@gmail.com" }, new CustomerDto() { Id = 3, Name = "Clara", Email = "Clara@gmail.com" }
+    };
+    [HttpGet]
+        public ActionResult<IEnumerable<CustomerDto>> Get()
+        {
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CustomerDto?> Get(int id)
+        {
+            CustomerDto? customer = customers.FirstOrDefault(customer => customer.Id == id);
+            if(customer == null) { return NotFound(); }
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] CustomerDto customer)
+        {
+            customers.Add(customer);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] CustomerDto updateCustomer)
+        {
+            CustomerDto? customer = customers.FirstOrDefault(customer => customer.Id == updateCustomer.Id);
+            if(customer == null) { return NotFound();}
+            customer.Name = updateCustomer.Name;
+            customer.Email = updateCustomer.Email;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if(customers.RemoveAll(customer => customer.Id == id) > 0) { return Ok(); }
+            return NotFound();
+        }
+    }
+}
---
+++ RestSharpTesterConsole/CustomerClient/CustomerRestClient.cs
@@ -0,0 +1,59 @@
+﻿using RestSharp;
+using RestSharpTesterConsole.CustomerClient.Model;
+
+namespace RestSharpTesterConsole.CustomerClient
+{
+    /// <summary>
+    /// This class implements the ICustomerClient and allows for CRUD on a REST service
+    /// which is located at the URL provided in the constructor
+    /// NOTE: this class hasn't implemented checks for errors in communication
+    /// </summary>
+    public class CustomerRestClient : ICustomerClient
+    {
+        RestClient _client;
+
+        public CustomerRestClient(string restUrl)
+        {
+            //instantiates and saves the RestSharp client
+            //for use in the CRUD methods
+            _client = new RestClient(restUrl);
+        }
+        public void DeleteCustomer(int id)
+        {
+            //sends a DELETE request to "api/customers/{id}"
+            var request = new RestRequest($"{id}", Method.Delete);
+            _client.Delete<CustomerDto>(request);
+        }
+
+        public CustomerDto? GetCustomerById(int id)
+        {
+            //sends a GET request to "api/customers/{id}"
+            var request = new RestRequest($"{id}");
+            return _client.Get<CustomerDto>(request);
+        }
+
+        public IEnumerable<CustomerDto>? GetCustomers()
+        {
+            //sends a GET request to "api/customers"
+            return _client.Get<IEnumerable<CustomerDto>>(new RestRequest());
+        }
+
+        public void InsertCustomer(CustomerDto customer)
+        {
+            //sends a POST request to "api/customers"
+            //with the Customer as a JSON object in body
+            var request = new RestRequest("", Method.Post);
+            request.AddBody(customer);
+            _client.Post<CustomerDto>(request);
+        }
+
+        public void UpdateCustomer(CustomerDto customer)
+        {
+            //sends a PUT request to "api/customers"
+            //with the Customer as a JSON object in body
+            var request = new RestRequest($"{customer.Id}");
+            request.AddBody(customer);
+            _client.Put<CustomerDto>(request);
+        }
+    }
+}
